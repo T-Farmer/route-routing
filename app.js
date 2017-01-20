112896c6fc4af059cf29378fd60c97e0dec26d1a
T-Farmer <terrill@Rels-Pro-186.local>
2017-01-20 10:01:30 -0600

    refactoring

--- app.js
+++ app.js
@@ -1,34 +1,67 @@
-console.log('Connected!');
-
-var app = angular.module('HighwayApp', ['ngRoute'])
-
-//configure the app
-
-app.config(($routeProvider) => {
-  $routeProvider
-    .when('/', {
-      controller: 'MainCtrl',
-      templateUrl: 'partials/index.html'
+angular
+  .module('HighwayApp', ['ngRoute'])
+  .config(($routeProvider) => {
+    $routeProvider
+    .when ('/', {
+      controller: 'RootCtrl',
+      templateUrl: '/partials/root.html',
     })
-    .when('/7mile', {
+    .when ('/7mile', {
       controller: 'SevenCtrl',
-      templateUrl: 'partials/7mile.html'
+      templateUrl: '/partials/7mile.html',
     })
-    .when('/pch', {
+    .when ('/Pch', {
       controller: 'PchCtrl',
-      templateUrl: 'partials/pch.html'
+      templateUrl: '/partials/pch.html'
     })
+  })
+
+.controller ('RootCtrl', function($scope) {
+  console.log('I am RootCtrl')
+  $scope.primary = () => 'main view'
 })
 
-//create controllers
-
-app.controller('MainCtrl', function ($scope) {
-  console.log("main controller!")
-  $scope.primary = "main view"
+.controller ('SevenCtrl', function($scope) {
+  console.log('I am SevenCtrl')
+  $scope.second = () => '7 Mile'
 })
 
-app.controller('SevenCtrl', function ($scope) {
 
-  console.log("Seven Mile Bridge!!")
-  $scope.second = "first bridge"
-})
+
+
+
+
+// console.log('Connected!');
+
+// var app = angular.module('HighwayApp', ['ngRoute'])
+
+// //configure the app
+
+// app.config(($routeProvider) => {
+//   $routeProvider
+//     .when('/', {
+//       controller: 'MainCtrl',
+//       templateUrl: 'partials/index.html'
+//     })
+//     .when('/7mile', {
+//       controller: 'SevenCtrl',
+//       templateUrl: 'partials/7mile.html'
+//     })
+//     .when('/pch', {
+//       controller: 'PchCtrl',
+//       templateUrl: 'partials/pch.html'
+//     })
+// })
+
+// //create controllers
+
+// app.controller('MainCtrl', function ($scope) {
+//   console.log("main controller!")
+//   $scope.primary = "main view"
+// })
+
+// app.controller('SevenCtrl', function ($scope) {
+
+//   console.log("Seven Mile Bridge!!")
+//   $scope.second = "first bridge"
+// })
